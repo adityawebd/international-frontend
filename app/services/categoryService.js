de437040c202ee3c65b27f5fbacce54993e559f0
Webdegre@6 <webdegredg@gmail.com>
2024-08-03 16:43:41 +0530

    avipc

--- app/services/categoryService.js
+++ app/services/categoryService.js
@@ -1,7 +1,7 @@
 // services/categoryService.js
 export const fetchCategoriesAndProducts = async () => {
-    const categoryRes = await fetch("http://internationalgift.in/api/category");
-    const productRes = await fetch("http://internationalgift.in/api/product");
+    const categoryRes = await fetch("/api/category");
+    const productRes = await fetch("/api/product");
   
     const categories = await categoryRes.json();
     const products = await productRes.json();
