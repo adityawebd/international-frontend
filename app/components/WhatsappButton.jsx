e5e75e8f4fcb09e53500bc1bb966937af9940162
Rapid Web devloper <webdegredg@gamil.com>
2024-09-02 17:22:17 +0530

    aditya pc

--- app/components/WhatsappButton.jsx
+++ app/components/WhatsappButton.jsx
@@ -7,7 +7,7 @@ const BackToTopButton = () => {
   return (
     <>
       <Link
-        href="https://wa.me/+918800217402"
+        href="https://wa.me/+918076361433"
         target='_blank'
         prefetch={true}
         className="whatsapp_button z-100 fixed bottom-8 right-8 p-2 rounded-full shadow-lg bg-green-500 text-white flex justify-center items-center"
