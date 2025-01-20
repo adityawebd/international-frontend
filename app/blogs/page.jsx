c19313884e8cb1399c9f085aa53bc4edd5ed9b88
Avinash-cloud <Avinash.work2000@gmail.com>
2025-01-20 18:08:18 +0530

    avi pc

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -33,7 +33,7 @@ export default function Blog() {
           setFilteredBlogs(result.data);
         }
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error("Error fetching data:", error); 
       }
     };
 
